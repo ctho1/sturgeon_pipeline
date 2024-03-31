673927d452f3ed1511614d9a0131d2370fbb14f8
Christian Thomas <c.thomas@uni-muenster.de>
2024-03-31 21:00:36 +0200

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -5,5 +5,5 @@ dirs=$(find ./pod5 -mindepth 1 -maxdepth 1 -type d -print|sort)
 for dir in $dirs; do
 	base=`basename $dir`
 	echo "submitting job ${base}"
-	sbatch ./scripts/slurm_script_pipeline.sh $dir
+	sbatch --job-name=${base}_basecalling ./scripts/gpu_basecalling.sh $dir
 done
