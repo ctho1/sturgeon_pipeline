67b8a16ad15343f8a4a893e5a4c213e668d3113a
Christian Thomas <c.thomas@uni-muenster.de>
2024-04-12 16:02:30 +0200

    Update run_sturgeon.sh

--- scripts/run_sturgeon.sh
+++ scripts/run_sturgeon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=18
+#SBATCH --cpus-per-task=12
 #SBATCH --partition normal
 #SBATCH --time=0:30:00
 #SBATCH --mem=16G
@@ -14,24 +14,6 @@ sturgeon_model=/scratch/tmp/thomachr/software/sturgeon/include/models/general.zi
 basedir="."
 base=`basename ${basedir}/$1`
 
-# QC hg38
-ml palma/2022a  GCC/11.3.0 SAMtools/1.16.1
-samtools sort ${basedir}/tmp/${base}/hg38/${base}_hg38_alignment.bam > ${basedir}/tmp/${base}/hg38/${base}_hg38_alignment_sorted.bam
-samtools index ${basedir}/tmp/${base}/hg38/${base}_hg38_alignment_sorted.bam
-NanoPlot -t 8 -c blue --N50 --bam ${basedir}/tmp/${base}/hg38/${base}_hg38_alignment_sorted.bam \
-	 -o ${basedir}/results/${base}/qc_hg38
-
-# QC chm13v2
-samtools sort ${basedir}/tmp/${base}/chm13v2/${base}_chm13v2_alignment.bam > ${basedir}/tmp/${base}/chm13v2/${base}_chm13v2_alignment_sorted.bam
-samtools index ${basedir}/tmp/${base}/chm13v2/${base}_chm13v2_alignment_sorted.bam
-NanoPlot -t 8 -c blue --N50 --bam ${basedir}/tmp/${base}/chm13v2/${base}_chm13v2_alignment_sorted.bam \
-	 -o ${basedir}/results/${base}/qc_chm13v2
-
-# CNV analysis
-ml palma/2020b GCC/10.2.0 OpenMPI/4.0.5 R/4.0.3
-Rscript ${basedir}/scripts/cnv_plot.R ${basedir}/tmp/${base}/hg38/ ${basedir}/results/${base}/cnv
-mv ${basedir}/results/${base}/cnv/cnv_plot_1000kbp.pdf ${basedir}/results/${base}
-
 # Sturgeon
 module unload
 ml palma/2021b GCC/11.2.0 OpenMPI/4.1.1 sturgeon/0.4.3
@@ -42,3 +24,16 @@ sturgeon predict \
 -o ${basedir}/results/${base} \
 --model-files $sturgeon_model \
 --plot-results
+
+# CNV analysis
+ml palma/2020b GCC/10.2.0 OpenMPI/4.0.5 R/4.0.3
+Rscript ${basedir}/scripts/cnv_plot.R ${basedir}/tmp/${base}/hg19/ ${basedir}/results/${base}/cnv
+mv ${basedir}/results/${base}/cnv/cnv_plot_1000kbp.pdf ${basedir}/results/${base}
+
+# QC hg19
+NanoPlot -t 8 -c blue --N50 --bam ${basedir}/tmp/${base}/hg19/${base}_hg19_alignment_sorted.bam \
+	 -o ${basedir}/results/${base}/qc_hg19
+
+# QC chm13v2
+NanoPlot -t 8 -c blue --N50 --bam ${basedir}/tmp/${base}/chm13v2/${base}_chm13v2_alignment_sorted.bam \
+	 -o ${basedir}/results/${base}/qc_chm13v2
